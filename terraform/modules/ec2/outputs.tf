b0335a1d79b58f4aa0b81dd0fdd421b44aef6063
unknown <asoares1@chewy.com>
2025-06-09 21:44:38 -0400

    remove all terraform

--- terraform/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_id" {
-  description = "The ID of the EC2"
-  value       = concat(aws_instance.discord_music_bot.*.id, [""])[0]
-}
-
-output "ec2_arn" {
-  description = "The ARN of the EC2"
-  value       = concat(aws_instance.discord_music_bot.*.arn, [""])[0]
-}
